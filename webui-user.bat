9276efeb7a2f7f738c75bc38e847481761148738
Stanislav Khlud <43534015+TheSuperiorStanislav@users.noreply.github.com>
2024-03-07 20:38:13 +0700

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--opt-sdp-attention --upcast-sampling --opt-channelslast
+set COMMANDLINE_ARGS=--use-directml --opt-sdp-attention --upcast-sampling --opt-channelslast
 
 call webui.bat
